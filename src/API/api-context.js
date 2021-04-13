4d12241fa6d0e8cd6a728fc1e67b1d1d669c02f3
Robbe Vrijenhoek <437402@student.fontys.nl>
2021-04-13 14:28:17 +0200

    api fix, kitchen page updates

---
+++ src/API/api-context.js
@@ -0,0 +1,36 @@
+import React, { useContext } from "react";
+import Api from "../API/api";
+
+const ApiContext = React.createContext({});
+
+const API = new Api();
+
+export function ApiProvider({ children }) {
+    
+    var contextValue = {
+        Api: API,
+        Ingredients: API.Ingredients,
+        Users: API.Users,
+        Roles: API.Roles,
+        Recipes: API.Recipes,
+        Kitchens: API.Kitchens,
+        RequirementsLists: API.RequirementsLists,
+        UnitTypes: API.UnitTypes,
+        IngredientCategories: API.IngredientCategories,
+        RecipeCategories: API.RecipeCategories,
+        GroceryLists: API.GroceryLists,
+        Custom: API.Custom,
+    };
+
+    console.log("Loading API...");
+
+    return (
+        <ApiContext.Provider value={contextValue}>
+            {children}
+        </ApiContext.Provider>
+    );
+};
+
+export function useAPI() {
+    return useContext(ApiContext);
+};
