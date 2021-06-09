91efe73123a642f3259ebf8df520ef3352cb7533
Robbe Vrijenhoek <437402@student.fontys.nl>
2021-06-09 23:38:40 +0200

    fix cypress tests protocol

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -7,10 +7,10 @@ beforeEach(() => {
         localStorage.setItem(ls_key, JSON.stringify(data));
         console.log('local account set.');
 
-        cy.intercept("GET", "https://localhost:5001/api/Authorize/Validate", { fixture: 'validate_token.json' }).as('validate');
-        cy.intercept("POST", "https://localhost:5001/api/Authorize/Refresh", { fixture: 'get_user_by_access_token.json' }).as('refresh');
-        cy.intercept("GET", "https://localhost:5001/api/Authorize/Me", { fixture: 'get_user_by_access_token.json' }).as('userbytoken');
-        cy.intercept("GET", "https://localhost:5001/api/authorize/login?ReturnUrl=https://localhost:3000/returnAuthorization", (req) => {
+        cy.intercept({ method: 'GET', url: /(.*?)\/api\/Authorize\/Validate(.*?)/ }, { fixture: 'validate_token.json' }).as('validate');
+        cy.intercept({ method: 'POST', url: /(.*?)\/api\/Authorize\/Refresh(.*?)/ }, { fixture: 'get_user_by_access_token.json' }).as('refresh');
+        cy.intercept({ method: 'GET', url: /(.*?)\/api\/Authorize\/Me(.*?)/ }, { fixture: 'get_user_by_access_token.json' }).as('userbytoken');
+        cy.intercept({ method: 'GET', url: /(.*?)\/\/api\/authorize\/login\?ReturnUrl=https:\/\/localhost:3000\/returnAuthorization/ }, (req) => {
             req.reply(404);
         }).as('login');
         
